c1679d496c763f6128a76de140cb6c3c47721038
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-03-08 20:00:56 -0600

    update: test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Layout } from '@components/layouts/Layout'
+import TextField from '@mui/material/TextField'
 
 export function Home() {
   return (
     <>
       <Layout>
         <h1>Svalinn</h1>
+        <TextField id='Name' label='Name' variant='outlined' />
       </Layout>
     </>
   )
